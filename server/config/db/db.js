7583f1f7dce62c7f633d216ceac4a219983c0b55
Kha <khaaxuannguyen@gmail.com>
2023-11-16 14:07:27 +0700

    First commit

---
+++ server/config/db/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function connectDB() {
+  // chuỗi kết nối kèm user-password - tên db - kèm lệnh ?retryWrites=true&w=majority
+  const url = "mongodb://localhost:27017/shop?retryWrites=true&w=majority";
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Đã kết nối tới mongodb thành công - shop Kha-Ty");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default connectDB;
